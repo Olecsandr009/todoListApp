724fd9f4c0347a4860d96f07e0c5ae33d4f6b834
Oleksandr123 <kirkovsasha009@gmail.com>
2024-02-07 14:28:27 +0200

    create controllers

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { createDTO } from './task.dto';
+import { TaskService } from './task.service';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private taskService: TaskService) {}
+
+  @Post('create-task')
+  async createTask(@Body() task: createDTO) {
+    return this.taskService.createTask(task);
+  }
+
+  @Get('get-tasks')
+  async getTasks() {
+    return this.taskService.find();
+  }
+
+  @Get('get-task/:_id')
+  async getTaskById(@Param('_id') _id: number) {
+    return this.taskService.findOne(_id);
+  }
+
+  @Get('delete-task/:_id')
+  async deleteTask(@Param('_id') _id: number) {
+    return this.taskService.deleteTask(_id);
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { RegisterDTO } from './user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('create-user')
+  async createUser(@Body() user: RegisterDTO) {
+    return await this.userService.registerUser(user);
+  }
+
+  @Get('get-users')
+  async getUsers() {
+    return await this.userService.find();
+  }
+
+  @Get('get-task/:_id')
+  async getUser(@Param('_id') _id: number) {
+    return await this.userService.findOne(_id);
+  }
+
+  @Get('delete-user/:_id')
+  async deleteUser(@Param('_id') _id: number) {
+    return await this.userService.deleteUser(_id);
+  }
+}
